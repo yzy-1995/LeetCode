cbb522bb63b968544fbf82b0abf89844f55ee879
yangzy <yzy15367422647@163.com>
2023-05-08 19:56:05 +0800

    提交

---
+++ src/com/yzy/simple/CountPrefixesofaGivenString.java
@@ -0,0 +1,39 @@
+package com.yzy.simple;
+
+/**
+ * ClassName: CountPrefixesofaGivenString
+ * Description:
+ * 给你一个字符串数组 words 和一个字符串 s ，其中 words[i] 和 s 只包含 小写英文字母 。
+ * 请你返回 words 中是字符串 s 前缀 的 字符串数目 。
+ * 一个字符串的 前缀 是出现在字符串开头的子字符串。子字符串 是一个字符串中的连续一段字符序列。
+ * 示例 1：
+ * 输入：words = ["a","b","c","ab","bc","abc"], s = "abc"
+ * 输出：3
+ * 解释：
+ * words 中是 s = "abc" 前缀的字符串为：
+ * "a" ，"ab" 和 "abc" 。
+ * 所以 words 中是字符串 s 前缀的字符串数目为 3 。
+ * 示例 2：
+ * 输入：words = ["a","a"], s = "aa"
+ * 输出：2
+ * 解释：
+ * 两个字符串都是 s 的前缀。
+ * 注意，相同的字符串可能在 words 中出现多次，它们应该被计数多次。
+ * @author Administrator
+ * @date 2023-5-8 11:28
+ */
+public class CountPrefixesofaGivenString {
+    public static void main(String[] args) {
+        System.out.println(countPrefixes(new String[]{"a", "b", "c", "ab", "bc", "abc"}, "abc"));
+    }
+
+    public static int countPrefixes(String[] words, String s) {
+        int count = 0;
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/com/yzy/simple/IntersectionofMultipleArrays.java
@@ -0,0 +1,44 @@
+package com.yzy.simple;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * ClassName: IntersectionofMultipleArrays
+ * Description:
+ * 给你一个二维整数数组 nums ，其中 nums[i] 是由 不同 正整数组成的一个非空数组，按 升序排列 返回一个数组，数组中的每个元素在 nums 所有数组 中都出现过。
+ * 示例 1：
+ * 输入：nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+ * 输出：[3,4]
+ * 解释：
+ * nums[0] = [3,1,2,4,5]，nums[1] = [1,2,3,4]，nums[2] = [3,4,5,6]，在 nums 中每个数组中都出现的数字是 3 和 4 ，所以返回 [3,4] 。
+ * 示例 2：
+ * 输入：nums = [[1,2,3],[4,5,6]]
+ * 输出：[]
+ * 解释：
+ * 不存在同时出现在 nums[0] 和 nums[1] 的整数，所以返回一个空列表 [] 。
+ * @author Administrator
+ * @date 2023-5-8 11:18
+ */
+public class IntersectionofMultipleArrays {
+    public static void main(String[] args) {
+
+    }
+
+
+    public List<Integer> intersection(int[][] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] num : nums) {
+            for (int i : num) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
+        }
+        return map.entrySet().stream()
+                .filter(e -> e.getValue() == nums.length)
+                .map(Map.Entry::getKey)
+                .sorted().collect(Collectors.toList());
+    }
+}
