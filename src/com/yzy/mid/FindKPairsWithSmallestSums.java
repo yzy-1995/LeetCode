adcf78c29e2b4ab263bf19aad9a99081d15bea51
yzy-1995 <865010107@qq.com>
2023-07-03 23:59:56 +0800

    提交

---
+++ src/com/yzy/mid/FindKPairsWithSmallestSums.java
@@ -0,0 +1,57 @@
+package com.yzy.mid;
+
+import java.util.*;
+
+/**
+ * @Author 杨泽宇
+ * @PackageName LeetCode
+ * @Package com.yzy.mid
+ * @Date 2023/07/03 23:40
+ * @Description 给定两个以 非递减顺序排列 的整数数组 nums1 和 nums2 , 以及一个整数 k 。
+ * <p>
+ * 定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2 。
+ * <p>
+ * 请找到和最小的 k 个数对 (u1,v1),  (u2,v2)  ...  (uk,vk) 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+ * 输出: [1,2],[1,4],[1,6]
+ * 解释: 返回序列中的前 3 对数：
+ * [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+ * 输出: [1,1],[1,1]
+ * 解释: 返回序列中的前 2 对数：
+ * [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ * 示例 3:
+ * <p>
+ * 输入: nums1 = [1,2], nums2 = [3], k = 3
+ * 输出: [1,3],[2,3]
+ * 解释: 也可能序列中所有的数对都被返回:[1,3],[2,3]
+ * @Version 1.0
+ */
+public class FindKPairsWithSmallestSums {
+    boolean flag = true;
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length, m = nums2.length;
+        if (n > m && !(flag = false)) return kSmallestPairs(nums2, nums1, k);
+        List<List<Integer>> ans = new ArrayList<>();
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> (nums1[a[0]] + nums2[a[1]]) - (nums1[b[0]] + nums2[b[1]]));
+        for (int i = 0; i < Math.min(n, k); i++) q.add(new int[]{i, 0});
+        while (ans.size() < k && !q.isEmpty()) {
+            int[] poll = q.poll();
+            int a = poll[0], b = poll[1];
+            ans.add(new ArrayList<>() {{
+                add(flag ? nums1[a] : nums2[b]);
+                add(flag ? nums2[b] : nums1[a]);
+            }});
+            if (b + 1 < m) q.add(new int[]{a, b + 1});
+        }
+        return ans;
+    }
+}
